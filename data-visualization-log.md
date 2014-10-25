f71ac51c1d9ebcd358e12aeb36dc8e03d4755fbf
cdelaguardia <dela3499@gmail.com>
2014-10-24 20:04:05 -0400

    Just debugged data mockup for distribution chart

--- data-visualization-log.md
+++ data-visualization-log.md
@@ -3,3 +3,33 @@ Apply the principles in the book to data visualization.
 
 # Hour 2 (4:17pm, Wed. Oct 22, 2014)
 Explore four subtasks. Ask questions and form goals. 
+
+# Hour 3 (7:05am, Thurs. Oct 23, 2014)
+Get chess data, and try to make some use of it. 
+
+# Hour 4 (8:21am, Thurs. Oct 23, 2014)
+Practice conceptual design process for a visualization.
+
+# Hour 5 (9:24am, Thurs. Oct 23, 2014)
+Investigate tools required to implement graphics.
+
+# Hour 6 (11:39am, Thurs. Oct 23, 2014)
+Deconstruct Bret Victor's graphic in The Ladder of Abstraction.
+
+# Hour 7 (2:10pm, Thurs. Oct 23, 2014)
+Design and (try to) mock up my chess visualization
+
+# Hour 8 (3:45pm, Thurs. Oct 23, 2014)
+Mock up my chess visualization
+
+# Hour 9 (8:32am, Friday Oct 24, 2014)
+Mock up the implementation for the graphic (Got distracted by playing chess)
+
+# Hour 10 (10:00am, Friday Oct 24, 2014)
+Identify data requirements
+
+# Hour 11 (1:00pm, Friday Oct 24, 2014)
+Mockup data and start working with a D3 example with a similar form
+
+# Hour 12 (5:40pm, Friday Oct 24, 2014)
+Finish mocking up data for material distribution chart
---
+++ src/dataMock.py
@@ -0,0 +1,68 @@
+import random
+import numpy
+
+def saturate (x, minimum, maximum):
+	if x < minimum:
+		return minimum
+	elif x > maximum:
+		return maximum
+	else:
+		return x
+
+def pointsTaken ():	
+	aggressiveness = random.choice([0,5,10])
+	piecesToLose = random.choice([0] * 10 + [1,3] * aggressiveness)
+	return piecesToLose
+
+# Bar Chart - distribution of games with different levels of material
+
+def distributionOfMaterial (nGames):
+	"Return arbitrary distribution of material for N games, for each move"
+
+	# Simulate the number pieces in gameplay
+	games = [simulateGame() for x in range(nGames)]
+	# Find length of longest game
+	longestGameLength = max([len(game["nWhitePoints"]) for game in games])
+
+	def initDict(games,nKeys):
+		"Return dictionary with properly-initialized keys"
+		return {(key + 1): 0 for key in range(nKeys)}
+	
+	# Initialize data structure to hold all the distributions
+	moveDists = [initDict(games,78) for move in range(longestGameLength)]
+
+	# Iterate through each move of each game, and increment counters
+	for game in games:
+		totalPoints = []
+		for move in zip(game["nWhitePoints"],game["nBlackPoints"]):
+			totalPoints.append(move[0] + move[1])
+		for move, nPoints in enumerate(totalPoints):
+			moveDists[move][nPoints] += 1
+
+	return moveDists
+
+def simulateGame ():
+	"""Return a list of integers representing 
+	the amount of material on the board for each player"""
+
+	# Randomly set length of game
+	gameLength = random.randint(20,40)
+	
+	# Randomly assign winner
+	gameWinner = random.choice(["white","black","draw"])
+
+	# Set up the board 
+	nWhitePoints = [39]
+	nBlackPoints = [39]
+
+	# Play game by randomly taking pieces
+	for move in range(gameLength-1):
+		w = nWhitePoints[move] # current number of pieces
+		b = nBlackPoints[move]
+		nWhitePoints.append(w - pointsTaken())
+		nBlackPoints.append(b - pointsTaken())
+
+	return {"nWhitePoints": nWhitePoints, "nBlackPoints": nBlackPoints}
+
+nGames = 100
+x = distributionOfMaterial (nGames)
