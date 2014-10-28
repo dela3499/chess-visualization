72024c323cc2cc93051bb92ba52903763fd7e2d8
cdelaguardia <dela3499@gmail.com>
2014-10-28 15:15:06 -0400

    Still in second refactor

--- src/elmChart.elm
+++ src/elmChart.elm
@@ -15,23 +15,35 @@ port moveInput = (scaleInput 50) <~ Mouse.x
 
 
 -- Chart --
-config: {h: number, w: number}
 config = { h = 800, w = 1500 }  
-
+{--
 makeBar: number -> number -> number -> Form
 makeBar i h w  = 
     let x = config.w / 2
         y = config.h / 2
     in filled red (rect w (h*7)) |> move (i * w - x , -y)
 
-makeChart: [number] -> [Form]
-makeChart d = 
-    let w  = config.w / toFloat(length d)
-        cb i h = makeBar i h w
-    in indexedMap cb d
+--}
+makeBar y w x h = filled red (rect w h) |> move (x,y)
+
+makeChart config dataset = 
+  let 
+    y = -config.height / 2
+    w = config.width / (length dataset)
     
-render: [number] -> Element
-render ds = collage config.w config.h (makeChart ds)
+    getX i = (i * w) - (config.width / 2)
+    xs = map getX [0 .. (length dataset - 1)]
+    
+    getH value = scale 50 value
+    hs = map getH dataset
+  in 
+    zipWith (makeBar y w) xs hs
+{--
+makeChart c d = 
+    let cb i h = makeBar i h (c.w / toFloat(length d))
+    in indexedMap cb d
+    --}
+render c d = collage c.w c.h (makeChart c d)
   
 main: Signal Element    
-main = lift render dataset
+main = (render config) <~ dataset
